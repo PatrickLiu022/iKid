c2d75e0c12ef93de2998b484a8e8534395acda54
patrickliu022 <patrickliu022@gmail.com>
2022-05-04 04:18:52 -0700

    finished good jokes

--- iKid/iKid/GoodJokesViewController.swift
+++ iKid/iKid/GoodJokesViewController.swift
@@ -11,12 +11,19 @@ class GoodJokesViewController: UIViewController {
     
     var jokeOneQuestionViewController: GoodJokesViewControllerQuestion! = nil
     var jokeOneAnswerViewController: GoodJokesViewControllerAnswer! = nil
+    var jokeTwoQuestionViewController: GoodJokesTwoViewControllerQuestion! = nil
+    var jokeTwoAnswerViewController: GoodJokesTwoViewControllerAnswer! = nil
+    var jokeThreeQuestionViewController: GoodJokeThreeViewControllerQuestion! = nil
+    var jokeThreeAnswerViewController: GoodJokeThreeViewControllerAnswer! = nil
+    
+    @IBOutlet weak var goodJokesLabel: UILabel!
     
     @IBAction func switchViews(_ sender: Any) {
-        print("Clicked next")
-//        jokeOneBuilder()
-        jokeOneQuestionBuilder()
-        jokeOneAnswerBuilder()
+        goodJokesLabel.isHidden = true
+        jokeOneBuilder()
+        jokeTwoBuilder()
+        jokeThreeBuilder()
+        
         UIView.beginAnimations("View Flip", context: nil)
         UIView.setAnimationDuration(0.4)
         UIView.setAnimationCurve(.easeInOut)
@@ -25,10 +32,26 @@ class GoodJokesViewController: UIViewController {
             UIView.setAnimationTransition(.flipFromRight, for: view, cache: true)
             jokeOneAnswerViewController.view.frame = self.view.frame
             switchViewController(jokeOneQuestionViewController, to: jokeOneAnswerViewController)
+        } else if jokeOneAnswerViewController != nil && jokeOneAnswerViewController?.view.superview != nil {
+            UIView.setAnimationTransition(.flipFromRight, for: view, cache: true)
+            jokeTwoQuestionViewController.view.frame = self.view.frame
+            switchViewController(jokeOneAnswerViewController, to: jokeTwoQuestionViewController)
+        } else if jokeTwoQuestionViewController != nil && jokeTwoQuestionViewController.view.superview != nil {
+            UIView.setAnimationTransition(.flipFromRight, for: view, cache: true)
+            jokeTwoAnswerViewController.view.frame = self.view.frame
+            switchViewController(jokeTwoQuestionViewController, to: jokeTwoAnswerViewController)
+        } else if jokeTwoAnswerViewController != nil && jokeTwoAnswerViewController.view.superview != nil {
+            UIView.setAnimationTransition(.flipFromRight, for: view, cache: true)
+            jokeThreeQuestionViewController.view.frame = self.view.frame
+            switchViewController(jokeTwoAnswerViewController, to: jokeThreeQuestionViewController)
+        } else if jokeThreeQuestionViewController != nil && jokeThreeQuestionViewController.view.superview != nil {
+            UIView.setAnimationTransition(.flipFromRight, for: view, cache: true)
+            jokeThreeAnswerViewController.view.frame = self.view.frame
+            switchViewController(jokeThreeQuestionViewController, to: jokeThreeAnswerViewController)
         } else {
             UIView.setAnimationTransition(.flipFromRight, for: view, cache: true)
-            jokeOneQuestionViewController.view.frame = view.frame
-            switchViewController(jokeOneAnswerViewController, to: jokeOneQuestionViewController)
+            jokeOneQuestionViewController.view.frame = self.view.frame
+            switchViewController(jokeThreeAnswerViewController, to: jokeOneQuestionViewController)
         }
         UIView.commitAnimations()
     }
@@ -58,6 +81,16 @@ class GoodJokesViewController: UIViewController {
         jokeOneAnswerBuilder()
     }
     
+    fileprivate func jokeTwoBuilder() {
+        jokeTwoQuestionBuilder()
+        jokeTwoAnswerBuilder()
+    }
+    
+    fileprivate func jokeThreeBuilder() {
+        jokeThreeQuestionBuilder()
+        jokeThreeAnswerBuilder()
+    }
+    
     fileprivate func jokeOneQuestionBuilder() {
         if jokeOneQuestionViewController == nil {
             jokeOneQuestionViewController =
@@ -73,6 +106,42 @@ class GoodJokesViewController: UIViewController {
                 storyboard?
                 .instantiateViewController(withIdentifier: "AnswerOne")
             as? GoodJokesViewControllerAnswer
+        }
+    }
+    
+    fileprivate func jokeTwoQuestionBuilder() {
+        if jokeTwoQuestionViewController == nil {
+            jokeTwoQuestionViewController =
+                storyboard?
+                .instantiateViewController(withIdentifier: "QuestionTwo")
+            as? GoodJokesTwoViewControllerQuestion
+        }
+    }
+    
+    fileprivate func jokeTwoAnswerBuilder() {
+        if jokeTwoAnswerViewController == nil {
+            jokeTwoAnswerViewController =
+                storyboard?
+                .instantiateViewController(withIdentifier: "AnswerTwo")
+            as? GoodJokesTwoViewControllerAnswer
+        }
+    }
+    
+    fileprivate func jokeThreeQuestionBuilder() {
+        if jokeThreeQuestionViewController == nil {
+            jokeThreeQuestionViewController =
+                storyboard?
+                .instantiateViewController(withIdentifier: "QuestionThree")
+            as? GoodJokeThreeViewControllerQuestion
+        }
+    }
+    
+    fileprivate func jokeThreeAnswerBuilder() {
+        if jokeThreeAnswerViewController == nil {
+            jokeThreeAnswerViewController =
+                storyboard?
+                .instantiateViewController(withIdentifier: "AnswerThree")
+            as? GoodJokeThreeViewControllerAnswer
         }
     }
     
